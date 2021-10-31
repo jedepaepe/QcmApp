8558a47f8a8cb35899dabdb8da3a04ec7639c643
jedepaepe <jedepaepe@epfc.eu>
2021-10-31 07:37:52 +0100

    scaffolding model

---
+++ QcmApp/Models/OptionsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QcmApp.Data;
+
+namespace QcmApp.Models
+{
+    public class OptionsController : Controller
+    {
+        private readonly QcmContext _context;
+
+        public OptionsController(QcmContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Options
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Option.ToListAsync());
+        }
+
+        // GET: Options/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var option = await _context.Option
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (option == null)
+            {
+                return NotFound();
+            }
+
+            return View(option);
+        }
+
+        // GET: Options/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Options/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Correct")] Option option)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(option);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(option);
+        }
+
+        // GET: Options/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var option = await _context.Option.FindAsync(id);
+            if (option == null)
+            {
+                return NotFound();
+            }
+            return View(option);
+        }
+
+        // POST: Options/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Correct")] Option option)
+        {
+            if (id != option.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(option);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OptionExists(option.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(option);
+        }
+
+        // GET: Options/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var option = await _context.Option
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (option == null)
+            {
+                return NotFound();
+            }
+
+            return View(option);
+        }
+
+        // POST: Options/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var option = await _context.Option.FindAsync(id);
+            _context.Option.Remove(option);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OptionExists(int id)
+        {
+            return _context.Option.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ QcmApp/Models/QuizzsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QcmApp.Data;
+
+namespace QcmApp.Models
+{
+    public class QuizzsController : Controller
+    {
+        private readonly QcmContext _context;
+
+        public QuizzsController(QcmContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Quizzs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Quizz.ToListAsync());
+        }
+
+        // GET: Quizzs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quizz = await _context.Quizz
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (quizz == null)
+            {
+                return NotFound();
+            }
+
+            return View(quizz);
+        }
+
+        // GET: Quizzs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Quizzs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title")] Quizz quizz)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(quizz);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(quizz);
+        }
+
+        // GET: Quizzs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quizz = await _context.Quizz.FindAsync(id);
+            if (quizz == null)
+            {
+                return NotFound();
+            }
+            return View(quizz);
+        }
+
+        // POST: Quizzs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title")] Quizz quizz)
+        {
+            if (id != quizz.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(quizz);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuizzExists(quizz.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(quizz);
+        }
+
+        // GET: Quizzs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quizz = await _context.Quizz
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (quizz == null)
+            {
+                return NotFound();
+            }
+
+            return View(quizz);
+        }
+
+        // POST: Quizzs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var quizz = await _context.Quizz.FindAsync(id);
+            _context.Quizz.Remove(quizz);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuizzExists(int id)
+        {
+            return _context.Quizz.Any(e => e.Id == id);
+        }
+    }
+}
